8221c12212a5a44b89b05dcb8d4dd9d67367267e
sham-l <shamz81443@gmail.com>
2024-09-07 17:13:33 +0530

    ui issues fixed added social buttons

--- src/components/socialLinks.tsx
+++ src/components/socialLinks.tsx
@@ -6,18 +6,18 @@ import React from 'react'
 const SocialLinks = ({ className }: { className: string }) => {
   return (
     <div className={cn("space-x-4 flex items-center text-slate-500 md:absolute ", className)}>
-    <Link className='overflow-hidden' href="https://www.linkedin.com/in/shameelk/">
+    <Link className='overflow-hidden' href="https://www.linkedin.com/in/shameelk/" target="_blank" rel="noopener noreferrer" >
      
         <Linkedin />
       
     </Link>
-    <Link className='overflow-hidden' href="https://github.com/sham-l">
+    <Link className='overflow-hidden' href="https://github.com/sham-l" target="_blank" rel="noopener noreferrer">
       <Github />
     </Link>
-    <Link className='overflow-hidden' href="mailto:shamz81443@gmail.com">
+    <Link className='overflow-hidden' href="https://mail.google.com/mail/?view=cm&to=shamz81443@gmail.com" target="_blank" rel="noopener noreferrer">
       <Gmail />
     </Link>
-    <Link className='overflow-hidden' href="tel:916238830867">
+    <Link className='overflow-hidden' href="tel:916238830867" rel="noopener noreferrer">
       <Phone />
     </Link>
       </div>
